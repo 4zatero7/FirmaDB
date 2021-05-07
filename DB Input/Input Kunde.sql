919fd065f6115a84bc650b757b6cca6c59110f46
Azat Erol <80638377+4zatero7@users.noreply.github.com>
2021-05-07 12:38:50 +0200

    Create Input Kunde.sql

---
+++ DB Input/Input Kunde.sql
@@ -0,0 +1,12 @@
+/************************
+@Author: Azat Erol
+Always happy coding!
+************************/
+
+INSERT INTO Kunde VALUES(500, 'DummyKunde1', 200);
+INSERT INTO Kunde VALUES(501, 'DummyKunde2', 200);
+INSERT INTO Kunde VALUES(502, 'DummyKunde3', 300);
+INSERT INTO Kunde VALUES(503, 'DummyKunde4', 300);
+INSERT INTO Kunde VALUES(504, 'DummyKunde5', 200);
+INSERT INTO Kunde VALUES(505, 'DummyKunde6', 300);
+INSERT INTO Kunde VALUES(506, 'DummyKunde7', 200);
