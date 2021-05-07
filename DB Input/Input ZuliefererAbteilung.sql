4721e8c875276a7a49340b1f19993d8c4741f79c
Azat Erol <80638377+4zatero7@users.noreply.github.com>
2021-05-07 12:38:17 +0200

    Create Input ZuliefererAbteilung.sql

---
+++ DB Input/Input ZuliefererAbteilung.sql
@@ -0,0 +1,12 @@
+/************************
+@Author: Azat Erol
+Always happy coding!
+************************/
+
+INSERT INTO ZuliefererAbteilung VALUES(200, 'ReinerZufallGmbH', 'Stifte');
+INSERT INTO ZuliefererAbteilung VALUES(200, 'Mustermann & Co. KG', 'Papier');
+INSERT INTO ZuliefererAbteilung VALUES(300, 'SchnellLogistik', 'Farbe');
+INSERT INTO ZuliefererAbteilung VALUES(200, 'A.E. Lieferung', 'Papier');
+INSERT INTO ZuliefererAbteilung VALUES(300, 'Transport Ltd.', 'Lebensmittel');
+INSERT INTO ZuliefererAbteilung VALUES(300, 'Fritz & Fische Service', 'Paper');
+INSERT INTO ZuliefererAbteilung VALUES(300, 'Herz Manifaktur', 'Möbel');
